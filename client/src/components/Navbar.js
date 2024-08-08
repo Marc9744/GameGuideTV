6034e62c8fdd427dc8cb840c530c089beaa24be6
Marc Kamradt <141109830+Marc9744@users.noreply.github.com>
2024-08-07 23:48:23 -0700

    added some images and navbar search

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/">GameGuideTV</Link>
+      </div>
+      <div className="navbar-search">
+        <a>Search</a>
       </div>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
--- node_modules/client/src/pages/Home.js
+++ node_modules/client/src/pages/Home.js
@@ -12,16 +12,20 @@ const Home = () => {
         <h2>Featured Games</h2>
         <div className="games-list">
           <div className="game-card">
-            <img src="path/to/valorant-image.jpg" alt="Valorant" />
+          <img src={`${process.env.PUBLIC_URL}/images/valorant.png`} alt="Valorant" />
             <h3>Valorant</h3>
           </div>
           <div className="game-card">
-            <img src="path/to/wow-image.jpg" alt="World of Warcraft" />
+            <img src={`${process.env.PUBLIC_URL}/images/worldofwarcraft.png`} alt="World of Warcraft" />
             <h3>World of Warcraft</h3>
           </div>
           <div className="game-card">
-            <img src="path/to/csgo-image.jpg" alt="CS:GO" />
+            <img src={`${process.env.PUBLIC_URL}/images/csgo.png`} alt="CS:GO" />
             <h3>CS:GO</h3>
+          </div>
+          <div className="game-card">
+            <img src={`${process.env.PUBLIC_URL}/images/leagueoflegends.png`} alt="League of Legends" />
+            <h3>League of Legends</h3>
           </div>
         </div>
       </div>
